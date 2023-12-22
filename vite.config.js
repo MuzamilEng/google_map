84b6e5ead59d0dc978c8b2d004fd0ff39f7f7753
Muzamil <124368687+MuzamilEng@users.noreply.github.com>
2023-12-22 18:56:00 +0500

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
+// import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    hmr:
-      process.env.CODESANDBOX_SSE || process.env.GITPOD_WORKSPACE_ID
-        ? 443
-        : undefined,
-  },
-});
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   server: {
+//     hmr:
+//       process.env.CODESANDBOX_SSE || process.env.GITPOD_WORKSPACE_ID
+//         ? 443
+//         : undefined,
+//   },
+// });
